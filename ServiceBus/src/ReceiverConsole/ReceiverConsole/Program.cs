828f45142f6c20be39f83741afd51d8d8dc2cde1
wseye <wouter.seye@gmail.com>
2013-04-05 22:20:13 +0200

    servicebus added

---
+++ ServiceBus/src/ReceiverConsole/ReceiverConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReceiverConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            const string queueName = "workerqueue";
+            var rand = new Random();
+
+            var mSender = QueueClient.CreateFromConnectionString(connString, queueName);
+            var mReceiver = QueueClient.CreateFromConnectionString(connString, queueName, ReceiveMode.PeekLock);
+
+            mReceiver.OnMessage(m =>
+                                    {
+                                        Thread.Sleep(rand.Next(1000, 3000));
+                                        Console.WriteLine("Received message {0}", m.GetBody<string>());
+                                        m.Complete();
+                                    }, 5);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string msg = string.Format("test{0}", i);
+                Console.WriteLine("Sending {0}", msg);
+                mSender.Send(new BrokeredMessage(msg));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
